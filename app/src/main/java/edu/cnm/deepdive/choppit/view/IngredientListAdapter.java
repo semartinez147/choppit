6f8a287a55632d0fa675607161cb8a86bfb96f2d
Samuel Martinez <semartinez147@gmail.com>
2020-03-09 11:22:18 -0600

    Improved ingredient list format, but removed ViewHolder implementation

--- app/src/main/java/edu/cnm/deepdive/choppit/view/IngredientListAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/choppit/view/IngredientListAdapter.java
@@ -29,40 +29,20 @@ public class IngredientListAdapter extends ArrayAdapter {
 
   @Nonnull
   public View getView (int position, View view, @Nonnull ViewGroup parent) {
-    Holder viewHolder;
-
     LayoutInflater inflater = context.getLayoutInflater();
-    View rowView = inflater.inflate(R.layout.edit_ingredient_list_item, parent, true);
-
-    viewHolder = new Holder(view);
-    viewHolder.measurement = (TextView) rowView.findViewById(R.id.measurement);
-    viewHolder.unit = (TextView) rowView.findViewById(R.id.unit);
-    viewHolder.name = (TextView) rowView.findViewById(R.id.name);
+    View rowView = inflater.inflate(R.layout.edit_ingredient_list_item, null, true);
 
     // working from https://www.javacodegeeks.com/2013/09/android-viewholder-pattern-example.html
 
-//    measurementTextField.setText(String.format("%.2f", measurement[position]));
-//    unitTextField.setText(unit[position]);
-//    nameTextField.setText(name[position]);
+    TextView measurementField = (TextView) rowView.findViewById(R.id.measurement);
+    TextView unitField = (TextView) rowView.findViewById(R.id.unit);
+    TextView nameField = (TextView) rowView.findViewById(R.id.name);
+
+    measurementField.setText(String.format("%.2f", measurement[position]));
+    unitField.setText(unit[position]);
+    nameField.setText(name[position]);
 
     return rowView;
-  }
-
-   static class Holder {
-
-    private final View view;
-    private TextView measurement;
-    private TextView unit;
-    private TextView name;
-
-    private Holder (@Nonnull View view) {
-      super();
-      this.view = view;
-      measurement = view.findViewById(R.id.measurement);
-      unit = view.findViewById(R.id.unit);
-      name = view.findViewById(R.id.name);
-    }
-
   }
 
 }
