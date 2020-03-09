e0fdcf3c7f76a33dc9a69b33f909f341cd862320
Samuel Martinez <semartinez147@gmail.com>
2020-03-09 09:02:20 -0600

    in progress.

--- app/src/main/java/edu/cnm/deepdive/choppit/view/IngredientListAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/choppit/view/IngredientListAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import edu.cnm.deepdive.choppit.R;
 import javax.annotation.Nonnull;
@@ -28,18 +29,40 @@ public class IngredientListAdapter extends ArrayAdapter {
 
   @Nonnull
   public View getView (int position, View view, @Nonnull ViewGroup parent) {
+    Holder viewHolder;
+
     LayoutInflater inflater = context.getLayoutInflater();
-    View rowView = inflater.inflate(R.layout.edit_ingredient_list_item, null, true);
+    View rowView = inflater.inflate(R.layout.edit_ingredient_list_item, parent, true);
 
-    TextView measurementTextField = (TextView) rowView.findViewById(R.id.measurement);
-    TextView unitTextField = (TextView) rowView.findViewById(R.id.unit);
-    TextView nameTextField = (TextView) rowView.findViewById(R.id.name);
+    viewHolder = new Holder(view);
+    viewHolder.measurement = (TextView) rowView.findViewById(R.id.measurement);
+    viewHolder.unit = (TextView) rowView.findViewById(R.id.unit);
+    viewHolder.name = (TextView) rowView.findViewById(R.id.name);
 
-    measurementTextField.setText(String.format("%.2f", measurement[position]));
-    unitTextField.setText(unit[position]);
-    nameTextField.setText(name[position]);
+    // working from https://www.javacodegeeks.com/2013/09/android-viewholder-pattern-example.html
+
+//    measurementTextField.setText(String.format("%.2f", measurement[position]));
+//    unitTextField.setText(unit[position]);
+//    nameTextField.setText(name[position]);
 
     return rowView;
+  }
+
+   static class Holder {
+
+    private final View view;
+    private TextView measurement;
+    private TextView unit;
+    private TextView name;
+
+    private Holder (@Nonnull View view) {
+      super();
+      this.view = view;
+      measurement = view.findViewById(R.id.measurement);
+      unit = view.findViewById(R.id.unit);
+      name = view.findViewById(R.id.name);
+    }
+
   }
 
 }
