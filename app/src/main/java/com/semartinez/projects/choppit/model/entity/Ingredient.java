a1294b5231f66cd6ba34eeb2e8f106719c2930c1
Samuel Martinez <semartinez147@gmail.com>
2020-08-14 09:37:50 -0600

    Refactored Ingredient.equals to avoid possible NPE.

--- app/src/main/java/com/semartinez/projects/choppit/model/entity/Ingredient.java
+++ app/src/main/java/com/semartinez/projects/choppit/model/entity/Ingredient.java
@@ -199,7 +199,7 @@ public class Ingredient {
       result = quantity.equals(other.quantity)
           && name.equals(other.name)
           && unit.equals(other.unit)
-          && (unitAlt == null && other.unitAlt == null) || (unitAlt.equalsIgnoreCase(other.unitAlt));
+          && unitAlt == null ? other.unitAlt == null : (unitAlt.equalsIgnoreCase(other.unitAlt));
     }
     return result;
   }
